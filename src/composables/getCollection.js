1f2e90af914a5a7c66deb46aa4f64aef612947c2
31000s <burakyabgu@gmail.com>
2023-11-30 04:44:11 +0300

    fix: Fix inclusion of createdBy function

--- src/composables/getCollection.js
+++ src/composables/getCollection.js
@@ -1,28 +1,28 @@
-import {ref} from 'vue'
+import { ref } from 'vue';
+import { db } from '../config/firebase';
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
 
-import {db} from '../config/firebase'
-import {collection, onSnapshot} from 'firebase/firestore'
 const getCollection = () => {
-    const documents = ref(null)
-    const error = ref(null)
+    const documents = ref(null);
+    const error = ref(null);
 
-    let collectionRef = collection(db, 'messages')
+    let collectionRef = collection(db, 'messages');
+    let q = query(collectionRef, orderBy('createdAt'));
 
-
-    onSnapshot(collectionRef,snap => {
-        let results = []
+    onSnapshot(q, snap => {
+        let results = [];
         snap.docs.forEach(doc => {
-            doc.data().createdAt && results.push({...doc.data(), id: doc.id})
-        })
-        documents.value = results
-        error.value = null
+            doc.data().createdAt && results.push({ ...doc.data(), id: doc.id });
+        });
+        documents.value = results;
+        error.value = null;
     }, err => {
-        console.log(err.message)
-        documents.value = null
-        error.value = 'Could not fetch data'
-    })
+        console.log(err.message);
+        documents.value = null;
+        error.value = 'Could not fetch data';
+    });
 
-    return {documents, error}
+    return { documents, error };
 }
 
-export default getCollection
+export default getCollection;
